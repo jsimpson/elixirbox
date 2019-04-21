8ea4efe1b8f207c4607c347c638ec7b39951ac1a
Jonathan Simpson <jsimpson.github@gmail.com>
2019-04-21 16:05:53 -0700

    playing with processes

--- misc/greeter.ex
+++ misc/greeter.ex
@@ -1,10 +1,23 @@
 defmodule Procs do
   # spawn(Procs, :greeter, [ "world" ])
-  # Hello, world
   # PID<0.425.0>
+  # Hello, world
+  #  def greeter(name) do
+  #  Process.sleep(1000)
+  #  IO.puts("Hello, #{name}")
+  #end
 
-  def greeter(name) do
-    Process.sleep(1000)
-    IO.puts("Hello, #{name}")
+  # spawn Procs, :greeter, []
+  # #PID<0.21848.3>
+  # iex(7)> pid = v(-1)
+  # PID<0.21848.3>
+  # iex(8)> send pid, "world"
+  # Hello, "world"
+  # "world"
+  def greeter(greeting) do
+    receive do
+      msg -> IO.puts("#{greeting}, #{msg}")
+    end
+    greeter(greeting)
   end
 end
